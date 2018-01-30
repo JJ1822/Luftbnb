e685598035c82382728ee514a724814b55c09599
Jay Johnson <jaysbeverage@gmail.com>
2018-01-30 14:43:04 -0800

    migrate user db, user model

---
+++ app/models/user.rb
@@ -0,0 +1,36 @@
+class User < ApplicationRecord
+  validates :email, :session_token, :first_name, presence: true
+  validates :email, uniqueness: true
+  validates :password, length: { minimum: 6 }, allow_nil: true
+
+  attr_reader :password
+
+  def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(password)
+  end
+
+  def is_password?(password)
+    BCrypt::Password.new(self.password_digest).is_password?(password)
+  end
+
+  def reset_session_token!
+    self.session_token = SecureRandom.urlsafe_base64
+    self.save!
+    self.session_token
+  end
+
+  def ensure_session_token
+    self.session_token ||= SecureRandom.urlsafe_base64
+  end
+
+  def self.find_by_cred(email, password)
+    user = User.find_by(email: email)
+    if user && user.is_password?(password)
+      return user
+    else
+      return nil
+    end
+  end
+
+end
