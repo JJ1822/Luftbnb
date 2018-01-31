56ce8d363c4aa0fd1fd753956e10e823e63fb214
Jay Johnson <jaysbeverage@gmail.com>
2018-01-31 09:59:04 -0800

    fixed importing issues, create and test store

--- frontend/reducers/session_reducer.js
+++ frontend/reducers/session_reducer.js
@@ -1,4 +1,4 @@
-import { RECEIVE_CURRENT_USER } from './actions/session_action';
+import { RECEIVE_CURRENT_USER } from '../actions/session_actions';
 import merge from 'lodash/merge';
 
 let initState = {
