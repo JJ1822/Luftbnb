940cd98a17c00f3e946f0b6b6b2bcc0fd3536c2c
Jay Johnson <jaysbeverage@gmail.com>
2018-01-31 17:00:31 -0800

    protected route

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   # protect_from_forgery with: :exception
-
+  helper_method :logged_in?
+  
   def current_user
     return nil unless session[:session_token]
     @current_user ||= User.find_by(session_token: session[:session_token])
