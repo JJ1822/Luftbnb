eb0d95053f8933846f8c0de7008aa4378885ec4e
Jay Johnson <jaysbeverage@gmail.com>
2018-02-04 14:30:59 -0800

    working map and hidden api key

--- frontend/components/spot/spot_map.jsx
+++ frontend/components/spot/spot_map.jsx
@@ -2,9 +2,17 @@ import React from 'react';
 
 class SpotMap extends React.Component {
 
+  componentDidMount() {
+    const mapOptions = {
+      center: { lat: 37.7758, lng: -122.453 },
+      zoom: 13
+    };
+    this.map = new google.maps.Map(this.mapNode, mapOptions);
+  }
+
   render() {
     return (
-      <div className="map-container" ref="map">
+      <div className="map-container" ref={ map => this.mapNode = map }>
 
       </div>
     );
