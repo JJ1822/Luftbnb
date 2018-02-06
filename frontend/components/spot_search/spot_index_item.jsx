7e4cd114eb4aab9d801cfc187a36cfebd0ac604e
Jay Johnson <jaysbeverage@gmail.com>
2018-02-06 11:34:41 -0800

    api key hidden

--- frontend/components/spot_search/spot_index_item.jsx
+++ frontend/components/spot_search/spot_index_item.jsx
@@ -3,21 +3,22 @@ import { Link } from 'react-router-dom';
 
 const SpotIndexItem = ({ spot }) => {
   return (
-    <div className="spot-index-item">
-        <img className="spot-index-image" src={spot.img_url}/>
-      <div className="spot-index-type">
-        <span>{spot.room_type}</span>
-        <span>{spot.num_beds}</span>
+    <Link to={`spots/${spot.id}`}>
+      <div className="spot-index-item">
+          <img className="spot-index-image" src={spot.img_url}/>
+        <div className="spot-index-type">
+          <span>{spot.room_type}</span>
+          <span>{spot.num_beds}</span>
+        </div>
+        <div className="spot-index-name">
+          <h2>Malibu Dream Airstream</h2>
+        </div>
+        <div className="spot-index-rating">
+          <span>From ${spot.price_per_day} per night</span>
+          <span>Rating</span>
+        </div>
       </div>
-      <div className="spot-index-name">
-        <h2>Malibu Dream Airstream</h2>
-      </div>
-      <div className="spot-index-rating">
-        <span>From ${spot.price_per_day} per night</span>
-        <span>Rating</span>
-      </div>
-
-    </div>
+    </Link>
   );
 };
 
