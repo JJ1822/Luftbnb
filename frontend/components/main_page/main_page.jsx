d74c5f0224d9486990ae43da2f51d3879b486b07
Jay Johnson <jaysbeverage@gmail.com>
2018-04-03 23:25:44 -0700

    mobile

--- frontend/components/main_page/main_page.jsx
+++ frontend/components/main_page/main_page.jsx
@@ -38,14 +38,14 @@ class MainPage extends React.Component {
         <div id="dropdown" className="header-list dropdown-content">
           <div className="link-item"><Link onClick={this.myFunction} to="/host">Become a host</Link></div>
           <div className="link-item"><Link onClick={this.myFunction} to="/">Trips</Link></div>
-          <div className="link-item last-item"><Link onClick={this.props.logout & this.myFunction} to="/">Log out</Link></div>
+          <div className="link-item last-item"><Link onClick={this.props.logout} to="/">Log out</Link></div>
         </div>
       );
     } else {
       return (
         <div id="dropdown" className="header-list dropdown-content">
-          <div className="link-item"><Link onClick={this.openModal & this.myFunction} to="/signup">Sign up</Link></div>
-          <div className="link-item last-item"><Link onClick={this.openModal & this.myFunction} to="/login">Log In</Link></div>
+          <div className="link-item"><Link onClick={this.openModal} to="/signup">Sign up</Link></div>
+          <div className="link-item last-item"><Link onClick={this.openModal} to="/login">Log In</Link></div>
         </div>
         );
       }
@@ -53,6 +53,7 @@ class MainPage extends React.Component {
 
   openModal() {
     this.setState({modalIsOpen: true});
+    this.myFunction();
   }
 
 
