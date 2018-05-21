b31c3512aa2c448f925a7c2ecededdd54709a565
Jay Johnson <jaysbeverage@gmail.com>
2018-05-20 23:36:00 -0500

    laptop format

--- frontend/components/spot_search/spot_index.jsx
+++ frontend/components/spot_search/spot_index.jsx
@@ -8,12 +8,12 @@ class SpotIndex extends React.Component {
   }
 
   render() {
-    $("ul div:last-child").addClass("last-item");
+    $("ul div:last-child").addClass("last-list-item");
     const { spots } = this.props;
     const spotItem = spots.map(spot => <SpotIndexItem key={spot.id} spot={spot} />);
     return (
         <div className="spot-item-grid">
-          <ul>
+          <ul className="ul">
             {spotItem}
           </ul>
         </div>
