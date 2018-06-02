219e8b3c1c32345694a501a8b76857558cecc1de
Jay Johnson <jaysbeverage@gmail.com>
2018-06-01 19:11:04 -0700

    search bar

---
+++ frontend/components/search_bar/search_bar.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+class SpotAutocomplete extends React.Component {
+
+  componentDidMount() {
+    var input = document.getElementById('autocomplete');
+    var options = {componentRestrictions: {country: 'us'}};
+    let autoComplete = new google.maps.places.Autocomplete(input, options);
+
+    autoComplete.addListener('place_changed', () => {
+      let place = autoComplete.getPlace();
+      let location = place.geometry.location;
+      this.props.update(location)
+    });
+  }
+
+
+
+  render() {
+    return (
+      <div className="autoComplete-form">
+        <input id="searchbar" placeholder="Search for a City"
+           type="text"></input>
+      </div>
+    );
+  }
+
+}
+
+export default SpotAutocomplete;
